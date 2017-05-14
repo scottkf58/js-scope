e37c7beda08f00991b2678d651adffb7aa81dcca
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-14 12:02:17 -1000

    first commit

--- scope.js
+++ scope.js
@@ -3,10 +3,12 @@ var isCookieJarOpen = null;
 
 function closeLid() {
   /* answer here */
+  isCookieJarOpen = false;
 }
 
 function openLid() {
   /* answer here */
+  isCookieJarOpen = true;
 }
 
 
@@ -17,7 +19,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + " " + world/* answer here */;
   }
   return innerFunction();
 }
@@ -31,8 +33,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix.length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
